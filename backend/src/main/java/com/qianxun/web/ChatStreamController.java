61e9be51ba14e3a87e34c5d004259fe8f31b6cf0
tianzhaoruixiang <tianzhaoruixiang666@gmail.com>
2026-05-07 00:56:35 +0800

    all

--- backend/src/main/java/com/qianxun/web/ChatStreamController.java
+++ backend/src/main/java/com/qianxun/web/ChatStreamController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.util.List;
 import java.util.concurrent.Executor;
 
 @RestController
